3d543158a1ed5f81752a761980d316d4e857899f
alexraduca01 <alex.raduca01@icloud.com>
2023-11-27 14:17:44 +0100

    Added scaffolding

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Badwords</title>
+    </head>
+
+    <body>
+        <form action="censor.php">
+            <textarea name="text" cols="30" rows="10"></textarea>
+            <input type="text" name="nome">
+            <button>Invia</button>
+        </form>    
+    </body>
+
+</html>
